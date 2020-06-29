2ef57b3e2c84b503bbfa573da9ffebd89db6bf6c
DjamelRAAB <djamel.r.75@gmail.com>
2020-06-29 14:42:18 +0200

    Add MoveIt version 0.0.4

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # MoveIt
-Projet annuel
+
+## Architecture
+
+.
+├── documentation
+│   └── doc.md
+├── moveit
+│   ├── __init__.py
+│   ├── dl.py
+│   └── ml.py
+├── README.md
+└── wheel
+    ├── MoveItPa-0.0.1-py3-none-any.whl
+    ├── MoveItPa-0.0.2-py3-none-any.whl
+    ├── MoveItPa-0.0.3-py3-none-any.whl
+    └── MoveItPa-0.0.4-py3-none-any.whl
--- use_case/test.py
+++ use_case/test.py
@@ -1,9 +1,11 @@
-from moveIt import *
+from moveit.ml import *
+from matplotlib import pyplot as plt
+
 
 if __name__== "__main__":
     directions = ["up", "right", "down", "left"]
-    move = moveIt(directions)
-    #print(move)
+    move = MoveIt_ML(directions)
+    print(move)
     move.set_moves()
     #x_train, y_train, x_test, y_test = move.split_data(img_size= (60, 105))
 
@@ -12,9 +14,9 @@ if __name__== "__main__":
     #model = move.train_model(x_train, y_train, validation_data=(x_test, y_test), dropout=0.2, 
     #    early_stopping=True, data_augmentation = True, epochs = 50, plot_confusion_matrix= True)
     
-    #model = move.load_model(newest_model_in_directory=True, validation_data=(x_test, y_test))
-    #plt.imshow(x_train[223])
+    #model = move.load_model(newest_model_in_directory=True)
+    #plt.imshow(x_train[0])
     #plt.show()
     #model = move.load_model(path_to_model='working/models/model_2020-05-14 00:45.h5', validation_data=(x_test, y_test))
     #model = move.load_model(newest_model_in_directory=True, path_to_model='working/models/model_2020-05-12 00:18.h5', validation_data=(x_test, y_test))
-    #move.record(model, 7)
+    #move.record(model, 15, 0.95)
